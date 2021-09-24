560599cc6f9c5bc57a6d9b2bb6e4cdc80b965212
visheshshukla <engr.visheshshukla@gmail.com>
2021-09-25 03:35:19 +0530

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,25 @@ import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 
 export class DishdetailComponent implements OnInit {
@@ -23,23 +37,24 @@ export class DishdetailComponent implements OnInit {
     comment: Comment;
     errMess: string;
     dishcopy: Dish;
+    visibility = 'shown';
 
-  formErrors = {
-    'author': '',
-    'comment': ''
-  };
+    formErrors = {
+      'author': '',
+      'comment': ''
+    };
 
-  validationMessages = {
-    'author': {
-      'required':      'Author Name is required.',
-      'minlength':     'Author Name must be at least 2 characters long.',
-      'maxlength':     'Author Name cannot be more than 25 characters long.'
-    },
-    'comment': {
-      'required':      'Comment is required.',
-      'minlength':     'Comment must be at least 1 characters long.'
-    }
-  };
+    validationMessages = {
+      'author': {
+        'required':      'Author Name is required.',
+        'minlength':     'Author Name must be at least 2 characters long.',
+        'maxlength':     'Author Name cannot be more than 25 characters long.'
+      },
+      'comment': {
+        'required':      'Comment is required.',
+        'minlength':     'Comment must be at least 1 characters long.'
+      }
+    };
   
     constructor(private dishservice: DishService,
       private route: ActivatedRoute,
@@ -52,10 +67,9 @@ export class DishdetailComponent implements OnInit {
     ngOnInit() {
       this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds,
         errmess => this.errMess = <any>errmess);
-        this.route.params
-        .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
-          errmess => this.errMess = <any>errmess );
+        this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); }))
+          .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+          errmess => this.errMess = <any>errmess);
     }
 
     setPrevNext(dishId: string) {
