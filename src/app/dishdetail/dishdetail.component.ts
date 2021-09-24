5542c0b6e45aa01cfb861511a2d44b72c17d85ef
visheshshukla <engr.visheshshukla@gmail.com>
2021-09-25 03:07:54 +0530

    HTTP Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -21,6 +21,7 @@ export class DishdetailComponent implements OnInit {
     next: string;
     commentForm: FormGroup;
     comment: Comment;
+    errMess: string;
 
   formErrors = {
     'author': '',
@@ -48,7 +49,8 @@ export class DishdetailComponent implements OnInit {
        }
   
     ngOnInit() {
-      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds,
+        errmess => this.errMess = <any>errmess);
       this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     }
